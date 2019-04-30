88ab4ea1cdcada6833e21ad1d47eb924f778be4c
venu rachakonda <venusays@gmail.com>
2019-04-30 02:58:19 +0000

    new functions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-AWS_BIN="/usr/bin/aws"
-OWNER_ID="747476456671"
+AWS_BIN="/usr/local/bin/aws"
+OWNER_ID="274941441461"
+NODE_SLEEP_INTERVAL=15
+NODE_INITIAL_WAIT_PERIOD=10
+
+
 
 function log() {
   echo "$(date -R ) $@"
@@ -12,10 +16,6 @@ function capture_old_launch_config() {
   SECURITY_GROUPS=$(${AWS_BIN} autoscaling describe-launch-configurations --launch-configuration-names ${LAUNCH_CONFIG_NAME} | jq -r '.LaunchConfigurations[].SecurityGroups[]')
   INSTANCE_TYPE=$(${AWS_BIN} autoscaling describe-launch-configurations --launch-configuration-names ${LAUNCH_CONFIG_NAME} | jq -r '.LaunchConfigurations[].InstanceType')
   USER_DATA=$(${AWS_BIN} autoscaling describe-launch-configurations --launch-configuration-names ${LAUNCH_CONFIG_NAME} | jq -r '.LaunchConfigurations[].UserData')
-}
-
-function capture_asg_nodes() {
-  nodes=$(aws autoscaling describe-auto-scaling-groups --region ${AWS_DEFAULT_REGION} --auto-scaling-group-name ${ASG_NAME} | jq -r '.AutoScalingGroups[].Instances[].InstanceId')
 }
 
 function create_image() {
@@ -66,4 +66,113 @@ function create_new_launch_configuration() {
 
 function update_asg_launch_configuration() {
   ${AWS_BIN} autoscaling update-auto-scaling-group --auto-scaling-group-name ${ASG_NAME} --launch-configuration-name "${APP_NAME}-${BUILD_NUMBER}-lc"
+}
+
+# AutoScalingGroups scaling
+
+function check_aws_connectivity() {
+  success="true"
+  while true; do
+    log "Checking connectivity to AWS API. "
+    RESULT=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --region ${AWS_DEFAULT_REGION}) || success="false"
+    [ "$success" = "false" ] && exit 5
+    [ "$success" = "true" ] && break
+  done
+}
+
+function capture_asg_nodes() {
+  nodes=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --region ${AWS_DEFAULT_REGION} --auto-scaling-group-name ${ASG_NAME} | jq -r '.AutoScalingGroups[].Instances[].InstanceId')
+  echo $nodes | tr " " "\n" > asg_nodes
+}
+
+
+function retry_timeout() {
+  max_retries=$1
+  current_retry=$2
+  task=$3
+
+  if [ ${current_retry} -ge ${max_retries} ]; then
+    log "Timeout. Max retries exceeded for " $task
+    exit 1
+  fi
+}
+
+
+###############
+
+function check_asg_state() {
+
+  node_count=$(cat asg_nodes | wc -l | tr -d ' ')
+  log "Total VMs in the ASG ${ASG_NAME} : $node_count"
+
+  desired_capacity=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name ${ASG_NAME} --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].DesiredCapacity')
+  min_size=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name ${ASG_NAME} --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].MinSize')
+  max_size=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name ${ASG_NAME} --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].MaxSize')
+  if [[ $max_size -lt $(( 2*desired_capacity )) ]]; then
+    log "${ASG_NAME} Max size ($max_size) should be >= 2x the desired capacity ($desired_capacity)."
+    double_max_size=$(( desired_capacity * 2 ))
+    log "Setting the Max size to ($double_max_size) for ${ASG_NAME}"
+    ${AWS_BIN} autoscaling update-auto-scaling-group --auto-scaling-group-name ${ASG_NAME} --region ${AWS_DEFAULT_REGION} --max-size $double_max_size
+  else
+    log "${ASG_NAME} Desired capacity $desired_capacity, Max size $max_size, Min size $min_size."
+  fi
+
+  current_instances=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name ${ASG_NAME} --region ${AWS_DEFAULT_REGION} | jq -r '.AutoScalingGroups[0].Instances[].LifecycleState' | wc -l | tr -d ' ')
+  if [[ $current_instances -ne $desired_capacity ]]; then
+    log "ERROR: ${ASG_NAME} may not be at its desired capacity. Current instances $current_instances. Desired instances: $desired_capacity"
+    exit 5
+  else
+    log "${ASG_NAME} is at its desired capacity. Current instances $current_instances. Desired instances: $desired_capacity"
+  fi
+}
+
+
+function double_capacity() {
+  group=${ASG_NAME}
+  capacity=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].DesiredCapacity')
+  desired=$(( capacity * 2 ))
+  log "Current desired capacity for $group is: $capacity, setting it to $desired"
+  ${AWS_BIN} autoscaling set-desired-capacity --auto-scaling-group-name $group --desired-capacity $desired --region ${AWS_DEFAULT_REGION}
+}
+
+function wait_for_desired_nodes() {
+  group=${ASG_NAME}
+  count=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq -r '.AutoScalingGroups[0].DesiredCapacity')
+  sleep $NODE_INITIAL_WAIT_PERIOD
+  retries=0
+  while true; do
+    updated_node_count=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --region ${AWS_DEFAULT_REGION} --auto-scaling-group-name ${ASG_NAME} | jq -r '.AutoScalingGroups[].Instances[].InstanceId' | wc -l | tr -d ' ')
+    msg="Waiting for nodes in ASG. Nodes found $updated_node_count. Expected nodes $count."
+    log $msg
+    [[  $updated_node_count -eq $count ]] && break
+    retry_timeout 200 $retries $msg
+    ((retries++))
+    sleep $NODE_SLEEP_INTERVAL
+  done
+}
+
+function reset_capacity() {
+  group=${ASG_NAME}
+  desired_capacity=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].DesiredCapacity')
+  min_size=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].MinSize')
+  max_size=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].MaxSize')
+
+  if [[ $max_size -eq $(( 2*desired_capacity )) ]]; then
+    log "Nothing to do. $group Desired capacity $desired_capacity, Max size $max_size, Min size $min_size."
+    exit 0
+  elif [[ $max_size -ne $desired_capacity ]]; then
+    log "$group Desired capacity $desired_capacity, Max size $max_size, Min size $min_size."
+  fi
+
+  log "Gradually resetting the desired capacity to max_size/2."
+  log "This is a DESTRUCTIVE process that will delete instances from the current cluster. Please Ctrl C in the next 20 seconds if you want to quit."
+  sleep 20
+  while true; do
+    desired_capacity=$(${AWS_BIN} autoscaling describe-auto-scaling-groups --auto-scaling-group-name $group --region ${AWS_DEFAULT_REGION} | jq '.AutoScalingGroups[].DesiredCapacity')
+    tmp_capacity=$(( desired_capacity - 1 ))
+    [[ $max_size -gt $(( 2*tmp_capacity )) ]] && break
+    log "Current desired capacity for $group is: $desired_capacity, setting it to $tmp_capacity"
+    ${AWS_BIN} autoscaling set-desired-capacity --auto-scaling-group-name $group --desired-capacity $tmp_capacity --region ${AWS_DEFAULT_REGION}
+    wait_for_desired_nodes
+  done
 }
